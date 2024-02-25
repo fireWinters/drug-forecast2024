72ab253925a07d08b734e3ebf7041af7933bea8f
唐王瑶 <tangwangyao@hualala.com>
2024-02-25 21:00:42 +0800

    feat: 把xgboost的模型预测展示在折线图，真实值和预测值

--- main.py
+++ main.py
@@ -6,7 +6,8 @@ from tqdm import tqdm
 import xgboost
 from sklearn.metrics import mean_squared_error, r2_score
 import matplotlib.pyplot as plt
-
+import matplotlib as mpl
+from sklearn.model_selection import train_test_split
 # 这个函数用于绘制散点图，并在图中加入一条45度对角线，方便比较真实值和预测值之间的关系。
 def scatter_plot_with_diagonal(y_true, y_pred):
     plt.figure(figsize=(8, 8))
@@ -17,6 +18,28 @@ def scatter_plot_with_diagonal(y_true, y_pred):
     plt.title('Scatter Plot of True vs. Predicted')
     plt.legend()
     plt.show()
+# 把xgboost的模型预测展示在折线图，真实值和预测值
+def scatter_plot_with_xgboost(y_true, y_pred):
+    # 设置agg.path.chunksize的值大于100
+    mpl.rcParams['agg.path.chunksize'] = 200
+   # 绘制预测值和真实值的折线对比图
+    plt.figure(figsize=(8, 8))
+    plt.plot(y_true, label='True values', marker='o',color='blue')
+    plt.plot(y_pred, label='Predicted values', marker='x',color='red')
+    plt.xlabel('Sample')
+    plt.ylabel('Value')
+    plt.title('XGBoost Predicted vs True Values')
+    plt.legend()
+    plt.show()
+    # plt.figure(figsize=(8, 6))
+    # plt.plot(y_true, label='True values', marker='o')
+    # plt.plot(y_pred, label='Predicted values', marker='x')
+    # plt.xlabel('Sample')
+    # plt.ylabel('Value')
+    # plt.title('XGBoost Predicted vs True Values')
+    # plt.legend()
+    # plt.grid(True)
+    # plt.show() 
 
 # 这个函数用于获取指定文件夹中的所有xlsx文件，并将每个文件中的每个sheet拼接成一个DataFrame。
 def get_all_dataframes():
@@ -137,7 +160,8 @@ if __name__ == '__main__':
     data = drug_category(data,fileB)
 
     data = data[~data['y_7'].isna()]  # 排除y的nan值
-
+    # y_7数据大于10000的，剔除
+    data = data[data['y_7'] < 10000]  
     X, y = data[['month', 'quarter', 'de_sum_1', 'de_mean_1', 'de_max_1', 'de_min_1', 'de_sum_3', 'de_mean_3', 'de_max_3', 'de_min_3',
                  'de_sum_7', 'de_mean_7', 'de_max_7', 'de_min_7', 'de_sum_14', 'de_mean_14', 'de_max_14', 'de_min_14',
                  'de_sum_30', 'de_mean_30', 'de_max_30', 'de_min_30', 'de_sum_60', 'de_mean_60', 'de_max_60', 'de_min_60',
@@ -145,9 +169,8 @@ if __name__ == '__main__':
     # 导出处理后的数据为CSV文件
     # data.to_csv('label_data.csv', index=False)
     # 导出带有药品分类代码的数据为CSV文件
-    data.to_csv('label_data_with_category.csv', index=False)
-    # y_7数据大于10000的，剔除
-    # data = data[data['y_7'] < 10000]    
+    # data.to_csv('label_data_with_category.csv', index=False)
+      
     # data.to_csv('label_data_lessthan10000.csv', index=False)
     # xgb_reg = xgboost.XGBRegressor()
     # xgb_reg.fit(X, y)
@@ -159,4 +182,34 @@ if __name__ == '__main__':
 
     # scatter_plot_with_diagonal(y, y_pred)
     # mes: 1098696.5627478997, r2: 0.80858310294179
+
+  
+    # 训练集和测试集的划分，使用train_test_split函数，将数据集划分为训练集和测试集，其中测试集占30%。   
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    # 初始化XGBRegressor
+    # model = xgboost.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.3, learning_rate = 0.1,
+                    # max_depth = 5, alpha = 10, n_estimators = 10)
+    # model=xgboost.XGBRegressor(objective ='reg:squarederror', colsample_bytree = 0.9, learning_rate = 0.1,
+    #                 max_depth = 5, alpha = 10, n_estimators = 10)
+    model=xgboost.XGBRegressor()
+    # 训练模型
+    model.fit(X_train, y_train)
+
+    # 预测
+    predictions = model.predict(X_test)
+
+    # 模型评估，使用均方根误差（RMSE）、MES和R^2来评估模型的性能。
+    rmse = np.sqrt(mean_squared_error(y_test, predictions))
+    mse=mean_squared_error(y_test, predictions)
+    r2=r2_score(y_test, predictions)
+    print("RMSE: %f" % (rmse), "MSE: %f" % (mse), "R^2: %f" % (r2))
+    # 没有剔除大于10000的数据时的结果：RMSE: 1678.441078 MES: 2817164.452318 R^2: 0.442721
+    # RMSE: 1678.441078 MES: 2817164.452318 R^2: 0.442721
+    # 剔除>10000数据
+    # RMSE: 498.584804 MSE: 248586.806396 R^2: 0.540677
+    scatter_plot_with_diagonal(y_test, predictions)
+    #colsample_bytree = 0.7 RMSE: 490.095843 MSE: 240193.934989 R^2: 0.556185
+    #colsample_bytree = 0.9 RMSE: 489.531457 MSE: 239641.047723 R^2: 0.557206
+    # scatter_plot_with_xgboost(y_test, predictions)
+
 
